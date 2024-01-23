d9e718a7c536035107177ac39a448df44104505c
Franciscaamponsah <franciscaamponsah24@gmail.com>
2024-01-23 10:52:30 +0000

    made changes

--- src/components/StagBtn.jsx
+++ src/components/StagBtn.jsx
@@ -5,8 +5,8 @@ const StagBtn = () => {
     <div className="flex space-x-4 ">
     <span className=""><img className="flex" src={symbol}/></span>
     <div className="space-y-4 ">
-    <button className="w-[12.563rem] flex justify-center items-center text-center text-[1rem] rounded-md bg-white font-medium leading-[1.5rem] tracking-[0.02rem] py-4 px-[0.75rem]">
-      <RiSearchLine />
+    <button className="w-[12.563rem] flex justify-center items-center text-center text-[1rem] rounded-md bg-yellow-600 font-medium leading-[1.5rem] tracking-[0.02rem] py-4 px-[0.75rem]">
+      <RiSearchLine className=" bg-red-600"/>
       Domain name search
     </button>
     <button className="w-[12.563rem] flex justify-center items-center text-center text-[1rem] rounded-md bg-white font-medium leading-[1.5rem] tracking-[0.02rem] py-4 px-[0.75rem]">
